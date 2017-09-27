b6e6c695bd49949735043b968a25c817d67fc19f
luyuanx2 <694436921@qq.com>
2017-09-27 17:13:00 +0800

    登录验证

---
+++ src/main/java/com/lyy/aspect/SellerAuthorizeAspect.java
@@ -0,0 +1,53 @@
+package com.lyy.aspect;
+
+import com.lyy.constant.CookieConstant;
+import com.lyy.constant.RedisConstant;
+import com.lyy.exception.SellerAuthorizeException;
+import com.lyy.utils.CookieUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 登录身份验证切面
+ * Created by luyuanyuan on 2017/9/27.
+ */
+@Aspect
+@Component
+@Slf4j
+public class SellerAuthorizeAspect {
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    @Pointcut("execution(public * com.lyy.controller.Seller*.*(..))" +
+            "&& !execution(public * com.lyy.controller.SellerUserController.*(..))")
+    public void verify() {}
+
+    @Before("verify()")
+    public void doVerify(){
+        ServletRequestAttributes requestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = requestAttributes.getRequest();
+        Cookie cookie = CookieUtil.get(request, CookieConstant.TOKEN);
+        if(cookie == null){
+            log.warn("【登录校验】Cookie中查不到token");
+            throw new SellerAuthorizeException();
+        }
+
+        String tokenValue = redisTemplate.opsForValue().get(String.format(RedisConstant.TOKEN_PREFIX, cookie.getValue()));
+        if(StringUtils.isEmpty(tokenValue)){
+            log.warn("【登录校验】Redis中查不到token");
+            throw new SellerAuthorizeException();
+        }
+    }
+}
---
+++ src/main/java/com/lyy/exception/SellerAuthorizeException.java
@@ -0,0 +1,7 @@
+package com.lyy.exception;
+
+/**
+ * Created by luyuanyuan on 2017/9/27.
+ */
+public class SellerAuthorizeException extends RuntimeException {
+}
